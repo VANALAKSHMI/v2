86b3593b1a72fd94b42dcf0719634a86820f2518
VANALAKSHMI <41038161+VANALAKSHMI@users.noreply.github.com>
2018-09-04 17:32:43 +0530

    Create v-7.py

---
+++ v-7.py
@@ -0,0 +1,11 @@
+#5
+print("to   find large" )
+x=int(input("enter the number:"))
+y=int(input("enter the number:"))
+z=int(input("enter the number:"))
+if(x>y):
+    print(x)
+elif(z>x):
+    print(z)
+else:
+    print(y)
