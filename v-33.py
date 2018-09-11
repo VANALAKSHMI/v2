8c669a16431968f0894eaf674e026b7e9d2b267b
VANALAKSHMI <41038161+VANALAKSHMI@users.noreply.github.com>
2018-09-11 16:34:34 +0530

    Create v-33.py

---
+++ v-33.py
@@ -0,0 +1,17 @@
+#23-TO FIND MINIMUM ELEMENT
+l=[]
+n=int(input("N="))
+if(1<=n<=100000):
+    print("enter your ",n,"integers")
+    for x in range(1,n+1):
+        a=int(input(""))
+        l.append(a)
+    print(l)
+    m=l[-1]
+    s=l[-1]
+    for x in l:
+        if(m>x):
+            m=x
+    print("minimum:",m)
+else:
+    print("invalid input")
