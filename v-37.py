c3378cd5eb4a076ee2056099ac4c0517a4a5103c
VANALAKSHMI <41038161+VANALAKSHMI@users.noreply.github.com>
2018-09-14 16:23:10 +0530

    Rename v-27.py to v-37.py

---
+++ v-37.py
@@ -0,0 +1,8 @@
+
+#27
+print("to validate a string is numeric or ")
+n=(input("N="))
+if(n.isalpha()==False)&(n.isdigit()==True):
+    print("the input is numeric")
+else:
+ print("the input is invalid")
