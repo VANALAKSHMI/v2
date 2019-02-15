464bbc92d00c525f33d87d842a4db41e99c8b8c6
VANALAKSHMI <41038161+VANALAKSHMI@users.noreply.github.com>
2019-02-15 18:32:20 +0530

    Create v-68.py

---
+++ v-68.py
@@ -0,0 +1,15 @@
+#58
+n=int(input(""))
+k=int(input(""))
+l=[]
+d=0
+for i in range(0,n):
+    a=int(input(""))
+    l.append(a)
+for x in l:
+    if(x==k):
+        d=d+1
+if(d>=1):
+    print("Yes")
+else:
+    print("no")
