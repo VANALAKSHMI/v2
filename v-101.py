025c0885d4433ee56711be8f24c548a8047b95e6
VANALAKSHMI <41038161+VANALAKSHMI@users.noreply.github.com>
2019-03-02 12:10:09 +0530

    Create v-101.py

---
+++ v-101.py
@@ -0,0 +1,10 @@
+#91
+#Total surface area of a cuboid = 2 ( l × b ) + ( l× h ) + ( b× h )
+#Volume of cuboid = length × breadth × height
+l=int(input(""))
+b=int(input(""))
+h=int(input(""))
+tsa=2*((l*b)+(l*h)+(b*h))
+print(tsa)
+v=l*b*h
+print(v)
